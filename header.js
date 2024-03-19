8f430b740754fb3b471940423cf7ed95d03b4bae
Raed Shorrosh <90145979+raedshorrosh@users.noreply.github.com>
2024-03-19 12:03:04 +0200

    Update header.js

--- header.js
+++ header.js
@@ -1,5 +1,5 @@
-   <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
-    <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+<script id="MathJax-script" async="" src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.2/MathJax.js?config=TeX-MML-AM_CHTML"></script>
+   <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
     <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
  
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
