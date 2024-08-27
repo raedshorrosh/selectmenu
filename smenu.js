cb39cb724fc21d46c3178f4a7ee58e1cfeb6ff93
Raed Shorrosh <90145979+raedshorrosh@users.noreply.github.com>
2024-08-27 19:09:45 +0300

    Update smenu.js

--- smenu.js
+++ smenu.js
@@ -1,7 +1,6 @@
 [[iframe]]
 
 [[script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js" /]]
-[[script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" /]]
 [[style href="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css" type="text/css" /]]
 
 [[style]]  .formula {text-align: left;}[[/style]
