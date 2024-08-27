b13a40454c095ec6bef7cc6e579d6a03f0afcedb
Raed Shorrosh <90145979+raedshorrosh@users.noreply.github.com>
2024-08-27 21:29:41 +0300

    Update smenu.js

--- smenu.js
+++ smenu.js
@@ -1,8 +1,8 @@
 [[iframe]]
-
+[[script  src="https://code.jquery.com/jquery-1.12.4.min.js" /]]
+[[script  src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"/]]
+[[style href="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css" type="text/css" /]]	
 [[script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js" /]]
-[[style href="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css" type="text/css" /]]
-
 [[style]]  .formula {text-align: left;}[[/style]
 
 <div id="dropdown1" style="width: 100px; height:100px">
